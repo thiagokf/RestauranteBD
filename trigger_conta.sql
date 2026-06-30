b42ec17f1670f15e5cb92547a20c659981774b70
Thiago Fusco <165811535+thiagokf@users.noreply.github.com>
2026-06-29 22:46:50 -0300

    Create trigger_conta.sql

---
+++ trigger_conta.sql
@@ -0,0 +1,55 @@
+-- Gerar Conta pós finalização do pedido
+CREATE OR REPLACE FUNCTION gerar_conta()
+RETURNS TRIGGER AS $$
+DECLARE
+    valorPedido NUMERIC(10,2);
+    taxa NUMERIC(10,2);
+    valorTotal NUMERIC(10,2);
+BEGIN
+
+    IF OLD.status <> 'Finalizado'
+       AND NEW.status = 'Finalizado' THEN
+  
+        -- Soma o valor de todos os itens do pedido
+        SELECT SUM(i.preco * ip.quantidade)
+        INTO valorPedido
+        FROM ItensPedido ip
+        JOIN Itens i
+            ON ip.item = i.id
+        WHERE ip.pedido = NEW.id;
+
+        -- Calcula 10% de serviço
+        taxa := valorPedido * 0.10;
+
+        -- Calcula o total
+        valorTotal := valorPedido + taxa;
+
+        -- Cria a conta
+        INSERT INTO Contas(
+            pedido,
+            valor_total,
+            valor_pedido,
+            taxa_servico
+        )
+        VALUES(
+            NEW.id,
+            valorTotal,
+            valorPedido,
+            taxa
+        );
+        
+        -- Atualiza comissão do garcom
+        UPDATE Garcons
+        SET comissao = comissao + taxa
+        WHERE codga = NEW.garcom;
+
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER gerarConta
+AFTER UPDATE OF status ON Pedidos
+FOR EACH ROW
+EXECUTE FUNCTION gerar_conta();

27a2dceedbedaacfbb1fd510c7490268fd34da3a
Thiago Fusco <165811535+thiagokf@users.noreply.github.com>
2026-06-29 22:46:06 -0300

    Create triggers_reserva.sql
    Updated comments for clarity in triggers_reserva.sql.

---
+++ triggers_reserva.sql
@@ -0,0 +1,54 @@
+-- Fazer reserva
+CREATE OR REPLACE FUNCTION fazer_reserva()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE Mesas
+    SET status = 'Reservada'
+    WHERE numero = NEW.mesa;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER fazerReserva
+AFTER INSERT ON Reservas
+FOR EACH ROW
+EXECUTE FUNCTION fazer_reserva();
+
+-- Concluir Reserva
+CREATE OR REPLACE FUNCTION concluir_reserva()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.status = 'Concluida' THEN
+        UPDATE Mesas
+        SET status = 'Ocupada'
+        WHERE numero = NEW.mesa;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER concluirReserva
+AFTER UPDATE OF status ON Reservas
+FOR EACH ROW
+EXECUTE FUNCTION concluir_reserva();
+
+-- Cancelar Reserva
+CREATE OR REPLACE FUNCTION cancelar_reserva()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.status = 'Cancelada' THEN
+    UPDATE Mesas
+    SET status = 'Disponivel'
+    WHERE numero = NEW.mesa;
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER cancelarReserva
+AFTER UPDATE of status ON Reservas
+FOR EACH ROW
+EXECUTE FUNCTION cancelar_reserva();
